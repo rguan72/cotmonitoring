33650b25784d05702ae63611369fc26042f41d1e
Richard Guan <rguan72@gmail.com>
2025-09-21 01:06:44 -0700

    .

--- judge.py
+++ judge.py
@@ -8,13 +8,13 @@ class Judge:
         load_dotenv()
         self.client = Together()
         SYSTEM_PROMPT = """
-        Given an LLM answer and a correct answer, determine if the LLM answer is correct.  Your response should be a single word: "correct" or "incorrect". If the llm_answer is rounded differently than the correct answer but otherwise correct, you should return "correct".
+        Given an LLM answer and a correct answer, determine if the LLM answer is correct.  Your response should be a single word: "correct" or "incorrect". If the llm_answer is rounded differently or has different units than the correct answer but otherwise correct, you should return "correct".
         """
         self.system_prompt = SYSTEM_PROMPT
 
     def judge(self, llm_answer: str, correct_answer: str) -> bool:
         response = self.client.chat.completions.create(
-            model="google/gemma-3n-E4B-it",
+            model="openai/gpt-oss-20b",
             messages=[
                 {"role": "system", "content": self.system_prompt},
                 {"role": "user", "content": f"LLM answer: {llm_answer}\nCorrect answer: {correct_answer}"}
